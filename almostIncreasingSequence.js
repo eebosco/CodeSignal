1017030de17c1f46a68fc66a27ca1e6d0aab9b55
eebosco <80177764+eebosco@users.noreply.github.com>
2021-03-06 20:38:20 -0300

    Update almostIncreasingSequence.js

--- almostIncreasingSequence.js
+++ almostIncreasingSequence.js
@@ -1,17 +1,33 @@
 function almostIncreasingSequence(sequence) {
-var ret = true;
-var newSeq;
-var orderedSeq;
-var uniqueSeq;
+
 function arrayEquals(a, b) {
     return Array.isArray(a) &&
       Array.isArray(b) &&
       a.length === b.length &&
       a.every((val, index) => val === b[index]);
 }
-if (sequence.length > 2) {
-    ret = false;
-    for (var i = 0; i < sequence.length; i++) {
+
+/*
+Si la cantidad de items de la secuencia es 1 o 2 devolver true
+*/
+if (sequence.length > 0 && sequence.length <= 2) return true;
+
+/*
+Si la cantidad de items unicos de la secuencia es menor 
+que la cantidad de items de la secuencia - 1 entonces
+devolver false
+*/
+const uniqueSequence = Array.from(new Set(sequence));
+if (uniqueSequence.length < (sequence.length - 1)) return false;
+
+/*
+Si la secuencia ordenada es igual a la secuencia devolver true
+*/
+const orderedSequence = [...sequence];
+orderedSequence.sort((a,b)=>a-b);
+if (arrayEquals(sequence, orderedSequence)) return true;
+
+for (var i = 0; i < sequence.length; i++) {
         newSeq = [...sequence];
         newSeq.splice(i, 1);
         orderedSeq = [...newSeq];
@@ -19,10 +35,9 @@ if (sequence.length > 2) {
         uniqueSeq = Array.from(new Set(orderedSeq));
         //console.log(newSeq + ' - ' + orderedSeq + ' - ' + uniqueSeq);
         if (arrayEquals(newSeq, orderedSeq) && arrayEquals(orderedSeq, uniqueSeq)) {
-            ret = true;
-            break;
+            return true;
         }
     }
-}
-return ret;
+
+return false;
 }
